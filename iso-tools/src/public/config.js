fe100117a504bf62022256a76fc6e677a580b6cc
Rivindu <rivindum.15@cse.mrt.ac.lk>
2025-06-24 11:27:35 +0530

    initial commit - webapp

---
+++ iso-tools/src/public/config.js
@@ -0,0 +1,14 @@
+const Config = {
+    APP_AUTH_CLIENT_ID: "gEZiH2v8dfmjwKTcHI4Ts9IEORoa",
+    APP_AUTH_BASE_URL: "https://api.asgardeo.io/t/wso2ob",
+    APP_AUTH_REDIRECT_BASE_URL: "http://localhost:3000",
+    BACKEND_BASE_URL: "http://localhost:9081",
+    
+    // Enable/Disable features
+    tools: {
+        MT_TO_MX: true,
+        MX_TO_MT: false,
+        ISO8583: false,
+        OPEN_BANKING_SANDBOX: true
+    }
+}
