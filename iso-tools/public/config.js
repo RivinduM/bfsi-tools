7c5646e0a819e52511e0b3890ee49b375622c2fe
Rivindu <rivindum.15@cse.mrt.ac.lk>
2025-06-25 18:28:50 +0530

    config update

--- iso-tools/public/config.js
+++ iso-tools/public/config.js
@@ -1,7 +1,7 @@
 const Config = {
     APP_AUTH_CLIENT_ID: "gEZiH2v8dfmjwKTcHI4Ts9IEORoa",
     APP_AUTH_BASE_URL: "https://api.asgardeo.io/t/wso2ob",
-    APP_AUTH_REDIRECT_BASE_URL: "http://localhost:3000",
+    APP_AUTH_REDIRECT_BASE_URL: "https://4a8a9ab6-235b-4ce4-85fc-96d9223c9bd1.e1-us-east-azure.choreoapps.dev",
      BACKEND_BASE_URL: "https://6755ee75-a57a-4518-8c81-d08d1d3fd10d-dev.e1-us-east-azure.choreoapis.dev/iso/backend-service/v1.0",
 //    BACKEND_BASE_URL: "http://localhost:9090/iso",
 
